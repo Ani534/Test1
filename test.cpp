903cc8463d901b5970ef5d4b6c8f44d8f3f20ea1
Ani534 <ani.bdeyan@gmail.com>
2025-02-21 13:01:09 +0400

    Update Hello NPUA GB

--- test.cpp
+++ test.cpp
@@ -2,25 +2,6 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
+  cout << "Hello National Polytechnic University of Gyumri branch!";
   return 0;
-}
-
-// Function to add two numbers
-int add(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    int num1, num2;
-
-    // Asking for user input
-    std::cout << "Enter two numbers: ";
-    std::cin >> num1 >> num2;
-
-    // Calling the function and displaying the result
-    int sum = add(num1, num2);
-    std::cout << "The sum is: " << sum << std::endl;
-
-    return 0;
 }
